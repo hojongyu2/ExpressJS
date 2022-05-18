125b1fa6b84c0440fe03fb45f2d13da6d06417a1
Hojong Yu <hojongyu@Hojongs-MBP.home>
2022-05-18 12:15:07 -0400

    .

--- routes/blogs.js
+++ routes/blogs.js
@@ -11,27 +11,37 @@ router.get('/', function (req, res, next) {
 router.get('/all', (req, res) => { 
     const sort = req.query.sort
     
+    //sort(function(a,b){ 
+    //     return b - a
+    // }) sort function that return it as descending order
+    // a - b is for ascending order
+
     if (sort === 'desc'){
         blogPosts.sort(function(a,b){
-            return Date.parse(new Date(b.createdAt)) - Date.parse(new Date(a.createdAt));
+            return new Date(b.createdAt) - new Date(a.createdAt);
         })
         res.json(blogPosts)
+        
     }else if (sort === 'asc'){
         blogPosts.sort(function(a,b){
-            return Date.parse(new Date(a.createdAt)) - Date.parse(new Date(b.createdAt));
+            return new Date(a.createdAt) - new Date(b.createdAt);
         });
         res.json(blogPosts)
     }
-    
+    //all?sort=desc
 });
-//all?sort=desc
+
 router.get('/:blogId', function (req, res, next) {
     const blogId = req.params.blogId;
-    const foundBlog = blogPosts[blogId];
-    res.json(foundBlog)
+    res.json(findBlogId(blogId))
 });
 
-
+function findBlogId (blogId){
+    const foundBlog = blogPosts.find((blog) => {
+        return blog.id === blogId
+    })
+    return foundBlog;
+}
 
 
 
