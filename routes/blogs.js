41c75733021ec026480966d783f66d14487fa867
Hojong Yu <hojongyu@Hojongs-MBP.home>
2022-05-20 17:10:18 -0400

    git delete

--- routes/blogs.js
+++ routes/blogs.js
@@ -4,12 +4,21 @@ var router = express.Router();
 var blogs = require("../public/sampleBlogs");
 const blogPosts = blogs.blogPosts;
 
+
+/*
+CRUD APP
+    Create - POST
+    Read - GET
+    Update - PUT
+    Delete - DELETE
+*/
+
 router.get('/', function (req, res, next) {
     res.json(blogPosts)
 });
 
-router.get('/all', (req, res) => { 
-    const sort = req.query.sort
+router.get('/all', (req, res, next) => { 
+    const sort = req.query.sort;
     
     //sort(function(a,b){ 
     //     return b - a
@@ -28,10 +37,12 @@ router.get('/all', (req, res) => {
         });
         res.json(blogPosts)
     }
+    res.json(blogPosts)
     //all?sort=desc
 });
 
-router.get('/singleBlog:blogId', function (req, res, next) {
+
+router.get('/singleBlog/:blogId', function (req, res, next) {
     const blogId = req.params.blogId;
     res.json(findBlogId(blogId))
 });
@@ -47,7 +58,6 @@ router.get('/postblog', (req, res, next) => {
     res.render('postBlog')
 });
 //when click submit, it will store it's value as body
-//req.body
 
 router.post('/submit', (req, res, next) => {
     console.log(req.body)
@@ -59,12 +69,35 @@ router.post('/submit', (req, res, next) => {
         createdAt: today.toISOString(),
         id: String(blogs.blogPosts.length + 1)
     }
+
     blogs.blogPosts.push(newPost);
     console.log(blogs.blogPosts);
 
     res.send('ok')
 
 });
+
+router.get('/displayblogs', (req, res, next) => {
+    res.render('displayBlogs')
+});
+
+router.get('/displaysingleblog', (req, res, next) => {
+    res.render('displaySingleBlog')
+});
+
+
+router.delete('/deletesingleblog/:blogId', (req, res, next) => {
+    const blogDelete = req.params.blogId;
+
+    for (let i = 0; i <blogPosts.length; i++){
+        let blog = blogPosts[i];
+        if (blog.id = blogDelete){
+            blogPosts.splice(i,1)
+        }
+    }
+
+    res.send('ok')
+})
 
 
 module.exports = router;
